0bc3a9760d8083314d6e053aba4bdb0f40959130
Necessities <nik.r060@mail.ru>
2024-07-26 17:33:48 +0300

    init

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -5,12 +5,12 @@ import {useTelegram} from "../../hooks/useTelegram";
 import {useCallback, useEffect} from "react";
 
 const products = [
-    {id: '1', title: 'Джинсы', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '2', title: 'Куртка', price: 12000, img: "1.jpeg", description: 'Зеленого цвета, теплая'},
-    {id: '3', title: 'Джинсы 2', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '4', title: 'Куртка 8', price: 122, description: 'Зеленого цвета, теплая'},
-    {id: '5', title: 'Джинсы 3', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '6', title: 'Куртка 7', price: 600, description: 'Зеленого цвета, теплая'},
+    {id: '1', title: 'Джинсы', price: 5000,img: "1.jpeg", description: 'Синего цвета, прямые'},
+    {id: '2', title: 'Маникюр', price: 12000, img: "2.jpeg", description: 'Зеленого цвета, теплая'},
+    {id: '3', title: 'Джинсы 2', price: 5000, img: "3.jpeg",description: 'Синего цвета, прямые'},
+    {id: '4', title: 'Куртка 8', price: 122,img: "4.jpeg", description: 'Зеленого цвета, теплая'},
+    {id: '5', title: 'Джинсы 3', price: 5000,img: "5.jpeg", description: 'Синего цвета, прямые'},
+    {id: '6', title: 'Куртка 7', price: 600,img: "6.jpeg", description: 'Зеленого цвета, теплая'},
     {id: '7', title: 'Джинсы 4', price: 5500, description: 'Синего цвета, прямые'},
     {id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая'},
 ]
