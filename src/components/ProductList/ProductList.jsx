a55f24a4fbd5bcb2cd4aec0f290aa94b5f544245
Necessities <nik.r060@mail.ru>
2024-07-26 15:15:05 +0300

    init7

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -6,7 +6,7 @@ import {useCallback, useEffect} from "react";
 
 const products = [
     {id: '1', title: 'Джинсы', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '2', title: 'Куртка', price: 12000, img: "../../photo/1.jpeg", description: 'Зеленого цвета, теплая'},
+    {id: '2', title: 'Куртка', price: 12000, img: "1.jpeg", description: 'Зеленого цвета, теплая'},
     {id: '3', title: 'Джинсы 2', price: 5000, description: 'Синего цвета, прямые'},
     {id: '4', title: 'Куртка 8', price: 122, description: 'Зеленого цвета, теплая'},
     {id: '5', title: 'Джинсы 3', price: 5000, description: 'Синего цвета, прямые'},
